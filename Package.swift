87464f042250d6390ff874487eb079ffed1d2c91
JD <jann.driessen@gmx.de>
2024-11-15 13:53:35 +0100

    add swift package

---
+++ Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version: 5.10
+
+import PackageDescription
+
+let package = Package(
+    name: "BasedTokenlists",
+    products: [
+        .library(name: "BasedTokenlists", targets: ["BasedTokenlists"]),
+    ],
+    targets: [
+        .target(
+            name: "BasedTokenlists",
+            resources: [.process("superchain-token-list-8453.json")]
+        ),
+    ]
+)
